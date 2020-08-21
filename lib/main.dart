5a78b5c1a6b41c4194d8ecfb4dcecbe2c606cfb2
Rajat Sharma <rajat.sharma4211@gmail.com>
2020-08-21 21:30:35 +0530

    removed safe area

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 100,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.blue,
-              ),
-            ],
-          ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              width: 100,
+              height: double.infinity,
+              color: Colors.blue,
+            ),
+          ],
         ),
       ),
     );
